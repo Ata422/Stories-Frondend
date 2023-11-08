ab707c6ee0896318f4aaafe6efc0cf971fb7040a
Ata422 <ataurrehmantechnicmentors@gmail.com>
2023-11-08 14:31:18 +0500

    backend

--- functions/api.js
+++ functions/api.js
@@ -10,7 +10,7 @@ router.get("/topstories/:category", async (req, res) => {
     try {
         const response = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${req.params.category}.json?api-key=zojBZKGv4I331mXjUkgNLI6A1lcgFpIa`);
         if (response.status === 200) {
-            res.json(response.data);
+            res.json(response.data)
         } else {
             res.status(response.status).json({ error: 'Failed to fetch data from New York Times API' });
         }
@@ -18,5 +18,6 @@ router.get("/topstories/:category", async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 });
+
 
 module.exports = router
